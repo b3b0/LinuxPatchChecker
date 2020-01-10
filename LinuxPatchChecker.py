1d3652a284c198a02f7acc042357901d81afd500
b3b0 <b3b0@users.noreply.github.com>
2020-01-10 12:55:08 -0600

    Update LinuxPatchChecker.py

--- LinuxPatchChecker.py
+++ LinuxPatchChecker.py
@@ -10,7 +10,8 @@ import getpass
 from socket import gaierror
 
 if len(sys.argv) < 5:
-    print("arguments missing")
+    print("Arguments missing.")
+    print("Please do the following: '$ python LinuxPatchChecker.py hostname/group port username command'")
     sys.exit(1)
 
 os.system("clear")
@@ -18,6 +19,7 @@ os.system("clear")
 username = sys.argv[3]
 command = sys.argv[4]
 port = sys.argv[2]
+#trust = sys.argv[5]
 groupServsarg = sys.argv[1]
 groupQuery = "/" + groupServsarg + ".srvs"
 thisPath = (os.getcwd())
@@ -25,6 +27,8 @@ fullGroupPath = (thisPath + groupQuery)
 isThisGroup = os.path.exists(fullGroupPath)
 
 def machine(hostname,username,password,command,port):
+    if command == "patchcheck":
+        command = '[ -f /etc/centos-release ] && { OUTPUT="$(yum check-update | grep updates | grep -v updates: | wc -l)"; echo "There are ${OUTPUT} updates available"; uptime;} && [ -f /etc/lsb_release ] && { /usr/lib/update-notifier/apt-check --human-readable; uptime;}'
     print("- - - - - - - - - - - - - - -")
     print("----------------------")
     print(hostname)
@@ -32,13 +36,13 @@ def machine(hostname,username,password,command,port):
     client = paramiko.SSHClient()
     try:
         client.load_system_host_keys()
-        client.set_missing_host_key_policy(paramiko.WarningPolicy)
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
         client.connect(hostname, port=port, username=username, password=password)
         stdin, stdout, stderr = client.exec_command(command)
         print(stdout.read())
         print("----------------------")
     except gaierror:
-        print("Can't connect to that, bub!")
+        print("Can't connect to that host! Does it exist? Are you online?")
     except AuthenticationException:
         print("Authentication failed, please verify your credentials")
     except SSHException:
@@ -49,11 +53,9 @@ def machine(hostname,username,password,command,port):
         client.close()
 def brag():
     print("""
-
 ###########################################################
 ## LinuxPatchChecker 1.0.4 - https://www.github.com/b3b0 ##
 ###########################################################
-
 """)
 brag()
 if isThisGroup == True:
